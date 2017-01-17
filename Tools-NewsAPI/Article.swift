601a423379f6205217958c8356401ef23f169234
allenchang <iamallenchang@gmail.com>
2017-01-17 20:08:03 +0800

    test

---
+++ Tools-NewsAPI/Article.swift
@@ -0,0 +1,19 @@
+//
+//  Article.swift
+//  Tools-NewsAPI
+//
+//  Created by AllenChang on 2017/1/17.
+//  Copyright © 2017年 AllenChang. All rights reserved.
+//
+
+import UIKit
+
+class Article: NSObject {
+    
+    var headline: String?
+    var desc: String?
+    var author: String?
+    var url: String?
+    var imageUrl: String?
+    
+}
